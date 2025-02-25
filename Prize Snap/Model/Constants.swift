712a5b035f41c1a926e23e52d13a3c64713e37a1
Edison Zheng <eddiezheng2678@gmail.com>
2025-02-25 13:55:26 -0500

    constants!

---
+++ Prize Snap/Model/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  Prize Snap
+//
+//  Created by Edison Zheng (student LM) on 2/25/25.
+//
+
+import SwiftUI
+
+struct Constants {
+    static let textFont: Font = Font(UIFont(name: "Verdana", size: 18) ?? UIFont.systemFont(ofSize: 12))
+    static let captionFont: Font = Font(UIFont(name: "Verdana", size: 15) ?? UIFont.systemFont(ofSize: 12))
+    static let titleFont: Font = Font(UIFont(name: "Verdana", size: 22) ?? UIFont.systemFont(ofSize: 12))
+}
+
+extension Color {
+    static let color = Color("base")
+}
