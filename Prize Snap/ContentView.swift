ec35f2ef0afda6ba4d491d0d30394dba625f7ce7
Edison Zheng <eddiezheng2678@gmail.com>
2025-02-25 13:30:24 -0500

    Initial Commit

---
+++ Prize Snap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Prize Snap
+//
+//  Created by Edison Zheng (student LM) on 2/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Prize Snap/Prize_SnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prize_SnapApp.swift
+//  Prize Snap
+//
+//  Created by Edison Zheng (student LM) on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Prize_SnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
