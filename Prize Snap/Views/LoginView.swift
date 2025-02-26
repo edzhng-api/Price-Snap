47dfaa4ce742e261af88f698377a43c713f6d5de
Filip <cyranfilip.cf@gmail.com>
2025-02-26 15:05:26 -0500

    Update LoginView.swift

--- Prize Snap/Views/LoginView.swift
+++ Prize Snap/Views/LoginView.swift
@@ -6,27 +6,68 @@
 //
 
 import SwiftUI
+import FirebaseAuth
 
 struct LoginView: View {
-    @State var username = ""
-    @State var password = ""
+    @EnvironmentObject var user: User
     var body: some View {
-        VStack {
-            Image("logoapp")
-                .resizable()
-                .scaledToFit()
-                .cornerRadius(360)
-            
-            TextField("Username", text: $username)
-                .padding(15)
-            
-            TextField("Password", text: $password)
-                .padding(.horizontal, 15)
-            
+        NavigationView {
+            VStack {
+                Text("LOGIN")
+                    .font(Constants.titleFont)
+                    .padding(.top)
+                Image("logoapp")
+                    .resizable().frame(width: 200, height: 200)
+                    .scaledToFit()
+                    .cornerRadius(360)
+                    .padding()
+                
+                TextField("Email", text: $user.email)
+                    .overlay(Rectangle().frame(height: 1).foregroundColor(.gray), alignment: .bottom)
+                    .padding(25)
+                
+                
+                SecureField("Password", text: $user.password)
+                    .overlay(Rectangle().frame(height: 1).foregroundColor(.gray), alignment: .bottom)
+                    .padding(.horizontal, 25)
+                
+                Spacer()
+                
+                Button(action: {
+                    Task {
+                        await loginUser()
+                    }
+                }, label: {
+                    Text("Login")
+                        .font(Constants.titleFont)
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 125)
+                        .padding(.vertical, 15)
+                        .background(Color.green)
+                        .cornerRadius(12)
+                })
+                
+                NavigationLink(destination: RegisterView()) {
+                    Text("Don't have an account yet?\n Create free account here. ")
+                        .foregroundColor(.black)
+                        .font(Constants.captionFont)
+                }
+                .padding(.bottom)
+                
+            }
+        }.navigationBarBackButtonHidden(true)
+    }
+    func loginUser() async {
+        do {
+            try await Auth.auth().signIn(withEmail: user.email, password: user.password)
+            user.isUserAuth = true
+        } catch let e as Error {
+            print(e)
         }
     }
 }
 
 #Preview {
     LoginView()
+        .environmentObject(User())
 }
