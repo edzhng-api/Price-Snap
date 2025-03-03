b2217f14166c53a03cb31be162bb951e67742d4f
Edison Zheng <eddiezheng2678@gmail.com>
2025-03-03 13:36:21 -0500

    Home screen updates + product class

--- Prize Snap/Views/HomeView.swift
+++ Prize Snap/Views/HomeView.swift
@@ -4,52 +4,89 @@
 //
 //  Created by Edison Zheng (student LM) on 2/26/25.
 //
-
 import SwiftUI
-
 struct HomeView: View {
+    @State var products: [Product] = [
+        Product(),
+        Product(),
+        Product()
+    ]
     var body: some View {
-        VStack {
-            Text("Home")
-                .font(Constants.titleFont)
-                .padding()
-            Spacer()
-            HStack {
-                Button(action: {
-                    
-                }, label: {
-                    Text("Cart")
-                        .font(Constants.textFont)
-                        .foregroundColor(.black)
-                })
-                .padding()
-                .background(Color.base)
-                .cornerRadius(10)
-                Button(action: {
-                    
-                }, label: {
-                    Text("Home")
-                        .font(Constants.textFont)
-                        .foregroundColor(.black)
-                })
-                .padding()
-                .background(Color.base)
-                .cornerRadius(10)
-                Button(action: {
-                    
-                }, label: {
-                    Text("Search Area")
-                        .font(Constants.textFont)
-                        .foregroundColor(.black)
-                })
-                .padding()
-                .background(Color.base)
-                .cornerRadius(10)
+        
+        ZStack {
+            VStack {
+                Text("Home")
+                    .font(Constants.titleFont)
+                    .padding()
+                ScrollView{
+                    VStack {
+                        ForEach(products) {product in
+                            HStack{
+                                Image(product.picture)
+                                    .resizable()
+                                    .frame(width: 150, height: 100)
+                                VStack {
+                                    Text(product.name)
+                                        .font(Constants.textFont)
+                                    Text(product.location)
+                                        .font(Constants.textFont)
+                                    Text("$" + String(product.price))
+                                        .font(Constants.textFont)
+                                }
+                            }
+                            Spacer()
+                        }
+                    }
+                }
+                Spacer()
+                HStack {
+                    Button(action: {
+                        
+                    }, label: {
+                        VStack {
+                            Image(systemName: "cart.fill")
+                                .foregroundColor(.black)
+                            Text("Cart")
+                                .font(Constants.captionFont)
+                                .foregroundColor(.black)
+                        }
+                    })
+                    .padding()
+                    .background(Color.base)
+                    .cornerRadius(10)
+                    Button(action: {
+                        
+                    }, label: {
+                        VStack {
+                            Image(systemName: "house.fill")
+                                .foregroundColor(.black)
+                            Text("Home")
+                                .font(Constants.captionFont)
+                                .foregroundColor(.black)
+                        }
+                    })
+                    .padding()
+                    .background(Color.base)
+                    .cornerRadius(10)
+                    Button(action: {
+                        
+                    }, label: {
+                        VStack {
+                            Image(systemName: "magnifyingglass")
+                                .foregroundColor(.black)
+                            Text("Search Area")
+                                .font(Constants.captionFont)
+                                .foregroundColor(.black)
+                        }
+                    })
+                    .padding()
+                    .background(Color.base)
+                    .cornerRadius(10)
+                }
             }
         }
     }
 }
-
 #Preview {
     HomeView()
 }
