bf261e8939db841884d321c6cf4f37e05b379927
Filip <cyranfilip.cf@gmail.com>
2025-03-06 12:54:05 -0500

    Update ContentView.swift

--- Prize Snap/Views/ContentView.swift
+++ Prize Snap/Views/ContentView.swift
@@ -9,13 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            HomeView()
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+            HomeView()
+                .tabItem {
+                    Image(systemName: "cart")
+                    Text("Cart")
+                }
+            SettingsView()
+                .tabItem {
+                    Image(systemName: "gear")
+                    Text("Settings")
+                }
+            
         }
-        .padding()
     }
 }
 
