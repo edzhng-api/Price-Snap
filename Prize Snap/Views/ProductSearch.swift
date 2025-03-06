68238d5b0069c6c7cab2537aefd4fe3a07803e39
Sebastian Steed <seb.steed@gmail.com>
2025-03-06 13:09:43 -0500

    ProductSearch

---
+++ Prize Snap/Views/ProductSearch.swift
@@ -0,0 +1,18 @@
+//
+//  ProductSearch.swift
+//  Prize Snap
+//
+//  Created by Sebastian Steed (student LM) on 3/6/25.
+//
+
+import SwiftUI
+
+struct ProductSearch: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductSearch()
+}
