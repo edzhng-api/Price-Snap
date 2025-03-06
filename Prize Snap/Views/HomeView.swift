132d60146fc4fa4b330f8b9cbcc228c4652ecc25
Filip <cyranfilip.cf@gmail.com>
2025-03-06 12:54:34 -0500

    Update HomeView.swift

--- Prize Snap/Views/HomeView.swift
+++ Prize Snap/Views/HomeView.swift
@@ -9,81 +9,89 @@ struct HomeView: View {
     @State var products: [Product] = [
         Product(),
         Product(),
+        Product(),
+        Product(),
+        Product(),
+        Product(),
+        Product(),
+        Product(),
+        Product(),
+        Product(),
+        Product(),
+        Product(),
         Product()
     ]
     var body: some View {
         
-        ZStack {
-            VStack {
-                Text("Home")
-                    .font(Constants.titleFont)
-                    .padding()
-                ScrollView{
-                    VStack {
-                        ForEach(products) {product in
-                            HStack{
-                                Image(product.picture)
-                                    .resizable()
-                                    .frame(width: 150, height: 100)
-                                VStack {
-                                    Text(product.name)
-                                        .font(Constants.textFont)
-                                    Text(product.location)
-                                        .font(Constants.textFont)
-                                    Text("$" + String(product.price))
-                                        .font(Constants.textFont)
-                                }
+        NavigationView {
+            ZStack {
+                VStack {
+                    HStack {
+                        Text("HOT DEALS")
+                            .padding(.leading,20)
+                            .bold()
+                            .font(Constants.titleFont)
+                        NavigationLink {
+                            EmptyView()
+                        } label: {
+                            HStack {
+                                Spacer()
+                                Text("See More")
+                                    .padding(.trailing,20)
+                                    .font(Constants.textFont)
                             }
-                            Spacer()
+                    }
+                    }
+                    
+                    ScrollView(.horizontal) {
+                        HStack {
+                            ForEach(products.prefix(10)) {product in
+                                VStack{
+                                    Image(product.picture)
+                                        .resizable()
+                                        .frame(width: 150, height: 100)
+                                    HStack {
+                                        Text(product.name)
+                                            .font(Constants.textFont)
+                                        Text("$" + String(product.price))
+                                            .font(Constants.textFont)
+                                    }
+                                }
+                                Spacer()
+                            }
+                        }
+                    }
+                    HStack {
+                        Text("For You")
+                            .bold()
+                            .font(Constants.titleFont)
+                            .padding(.leading,20)
+                        Spacer()
+                        
+                    }
+                    ScrollView{
+                        VStack {
+                            ForEach(products) {product in
+                                HStack{
+                                    Image(product.picture)
+                                        .resizable()
+                                        .frame(width: 150, height: 100)
+                                    VStack {
+                                        Text(product.name)
+                                            .font(Constants.textFont)
+                                        Text(product.location)
+                                            .font(Constants.textFont)
+                                        Text("$" + String(product.price))
+                                            .font(Constants.textFont)
+                                    }
+                                }
+                                Spacer()
+                            }
                         }
                     }
                 }
-                Spacer()
-                HStack {
-                    Button(action: {
-                        
-                    }, label: {
-                        VStack {
-                            Image(systemName: "cart.fill")
-                                .foregroundColor(.black)
-                            Text("Cart")
-                                .font(Constants.captionFont)
-                                .foregroundColor(.black)
-                        }
-                    })
-                    .padding()
-                    .background(Color.base)
-                    .cornerRadius(10)
-                    Button(action: {
-                        
-                    }, label: {
-                        VStack {
-                            Image(systemName: "house.fill")
-                                .foregroundColor(.black)
-                            Text("Home")
-                                .font(Constants.captionFont)
-                                .foregroundColor(.black)
-                        }
-                    })
-                    .padding()
-                    .background(Color.base)
-                    .cornerRadius(10)
-                    Button(action: {
-                        
-                    }, label: {
-                        VStack {
-                            Image(systemName: "magnifyingglass")
-                                .foregroundColor(.black)
-                            Text("Search Area")
-                                .font(Constants.captionFont)
-                                .foregroundColor(.black)
-                        }
-                    })
-                    .padding()
-                    .background(Color.base)
-                    .cornerRadius(10)
-                }
-            }
+            }.navigationTitle("Home")
+                .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
