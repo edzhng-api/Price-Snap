5a76b993f6ac89d61bd9c09461be5f2531e48425
Filip <cyranfilip.cf@gmail.com>
2025-03-07 13:27:08 -0500

    Update User.swift

--- Prize Snap/Model/User.swift
+++ Prize Snap/Model/User.swift
@@ -10,12 +10,13 @@ import Foundation
 class User: ObservableObject {
     @Published var email: String
     @Published var password: String
-    @Published var isUserAuth: Bool
+    @Published var username: String
+    @Published var isUserAuth: Bool = false
     
     
-    init(email: String = "", password: String = "", isUserAuth: Bool = false) {
+    init(email: String = "", password: String = "", username: String = "") {
         self.email = email
         self.password = password
-        self.isUserAuth = isUserAuth
+        self.username = username
     }
 }
