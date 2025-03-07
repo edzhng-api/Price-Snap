82739f97599052a80d7e022cb281528a0be7611f
Filip <cyranfilip.cf@gmail.com>
2025-03-07 13:25:32 -0500

    Update ContentView.swift

--- Prize Snap/Views/ContentView.swift
+++ Prize Snap/Views/ContentView.swift
@@ -6,8 +6,10 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
+    @State private var permissionGranted = false
     var body: some View {
         TabView {
             HomeView()
@@ -26,10 +28,54 @@ struct ContentView: View {
                     Text("Settings")
                 }
             
+        }.onAppear {
+            checkNotificationPermissions()
+        }
+        .onChange(of: permissionGranted) { _ in
+            if permissionGranted {
+                scheduleNotification()
+            }
         }
     }
+    
+    private func checkNotificationPermissions() {
+            UNUserNotificationCenter.current().getNotificationSettings { settings in
+                if settings.authorizationStatus == .authorized {
+                    permissionGranted = true
+                } else {
+                    requestNotificationPermissions()
+                }
+            }
+        }
+
+    private func requestNotificationPermissions() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+            if success {
+                permissionGranted = true
+            } else {
+                print("Notification permission denied: \(error?.localizedDescription ?? "Unknown error")")
+            }
+        }
+    }
+    private func scheduleNotification() {
+        let notificationContent = UNMutableNotificationContent()
+        notificationContent.title = "Reminder"
+        notificationContent.body = "This is a reminder!"
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 60, repeats: true)
+
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: notificationContent, trigger: trigger)
+
+        UNUserNotificationCenter.current().add(request) { error in
+            if let error = error {
+                print("Failed to schedule notification: \(error.localizedDescription)")
+            }
+        }
+    }
+
 }
 
 #Preview {
     ContentView()
+        .environmentObject(User())
 }
