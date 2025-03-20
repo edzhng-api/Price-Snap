b8805c2efe5fcf948350ac65f2b24664142727cd
Filip <cyranfilip.cf@gmail.com>
2025-03-20 14:06:57 -0400

    Update Product.swift

--- Prize Snap/Model/Product.swift
+++ Prize Snap/Model/Product.swift
@@ -7,19 +7,33 @@
 //  Created by Edison Zheng (student LM) on 2/26/25.
 //
 import Foundation
-struct Product: Identifiable, Equatable {
+
+struct Product: Codable, Identifiable {
     let id = UUID()
-    var name: String
-    var price: Double
-    var location: String
-    var quantity: Int
-    var picture: String
+    let name: String
+    let price: Double
+    let link: String
+    let image: String
+    let store: String
     
-    init(name: String = "Name", price: Double = 0.00, location: String = "Location Not Found", quantity: Int = 1, picture: String = "Placeholder") {
+    init(name: String, price: Double, link: String, image: String, store: String) {
         self.name = name
         self.price = price
-        self.location = location
-        self.quantity = quantity
-        self.picture = picture
+        self.link = link
+        self.image = image
+        self.store = store
+    }
+    
+    var formattedPrice: String {
+        return String(format: "$%.2f", price)
     }
 }
+
+struct Store: Codable {
+    var name: String 
+    let products: [Product]
+}
+
+
+
+
