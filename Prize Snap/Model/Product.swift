beb60c2fcc30bf56f7312f5e3f89836416602b45
Filip <cyranfilip.cf@gmail.com>
2025-03-28 12:42:02 -0400

    Update Product.swift

--- Prize Snap/Model/Product.swift
+++ Prize Snap/Model/Product.swift
@@ -8,14 +8,14 @@
 //
 import Foundation
 
-struct Product: Codable, Identifiable {
-    let id = UUID()
-    let name: String
-    let price: Double
-    let link: String
-    let image: String
-    let store: String
-    
+struct Product: Identifiable, Codable, Equatable {
+    var id = UUID()
+    var name: String
+    var price: Double
+    var link: String
+    var image: String
+    var store: String
+
     init(name: String, price: Double, link: String, image: String, store: String) {
         self.name = name
         self.price = price
@@ -23,15 +23,34 @@ struct Product: Codable, Identifiable {
         self.image = image
         self.store = store
     }
+
+    enum CodingKeys: String, CodingKey {
+        case name, price, link, image, store
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.name = try container.decode(String.self, forKey: .name)
+        self.price = try container.decode(Double.self, forKey: .price)
+        self.link = try container.decode(String.self, forKey: .link)
+        self.image = try container.decode(String.self, forKey: .image)
+        self.store = try container.decode(String.self, forKey: .store)
+
+        self.id = UUID()
+    }
     
     var formattedPrice: String {
         return String(format: "$%.2f", price)
     }
 }
 
+
+
+
 struct Store: Codable {
-    var name: String 
-    let products: [Product]
+    let name: String
+    var products: [Product]
 }
 
 
